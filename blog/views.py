2a1ce8bb7878f9a6bd08798efc802501fb0ebce5
bogdan <bogdandumitrescu@yahoo.com>
2023-06-18 12:43:43 -0600

    Initialized home page with welcome message

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def starting_page(request):
-    pass
+    return render(request, "blog/index.html")
 
 def posts(request):
     pass
